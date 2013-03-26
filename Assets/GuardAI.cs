5b36a375c44667b95c4ed6ba77a8c5f7a8f3709f
Justin White <jrw152@case.edu>
2013-03-26 12:50:21 -0400

    created a guard that will follow the closest prisoner

---
+++ Assets/GuardAI.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class GuardAI : MonoBehaviour {
+	GameObject[] gos;
+	GameObject closest;
+	int speed = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        gos = GameObject.FindGameObjectsWithTag("player_shank");
+        
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos) {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance) {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+		Transform playerTransform = closest.transform;
+		// get player position
+		Vector3 v1 = playerTransform.position;
+         transform.position = Vector3.MoveTowards(transform.position, v1, speed*Time.deltaTime);
+	}
+}
