3f8472121c7c198ad00e81d6f2e8790f3458371f
Andy Lin <abl38@case.edu>
2013-04-04 15:33:06 -0400

    commented my code

--- Assets/mousePointer.cs
+++ Assets/mousePointer.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 using System.Collections;
-
+// ABL38 - mousePointer is for the code of the white circle that appears whenever you click any spot on the map
+// it represents the spot of the position that the mouse clicked and disappears accordingly
+// all code here is made by ABL38 - Andy Lin
 public class mousePointer : MonoBehaviour {
+	// specifies whether the mousePointer should disappear or not
 	public static bool disappear = true;
+	// the particles that explode every time you click
 	public GameObject particleSystem;
+	// the sound of the click
 	public AudioClip clicked;
+	// if the mouse is clicked or not
 	public static bool mouseIsClicked = false;
 	// Use this for initialization
 	void Start () {
@@ -13,20 +19,29 @@ public class mousePointer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// if the mouse is clicked, call the Clicked function
 		if(mouseIsClicked){
 			SendMessage ("Clicked");
 		}
+		// if the disappear is set to true, make the mousePointer invisible
 		if(disappear){
 			renderer.enabled = false;
 		}
 	}
 	void Clicked(){
+		// play the click sound
 		audio.PlayOneShot(clicked);
+		// create a Vector3 variable that holds the position of the clicked sot
 		Vector3 mousePos = Input.mousePosition;
+		// the z is set to the difference of the position to the camera
 		mousePos.z = transform.position.z - Camera.mainCamera.transform.position.z;
+		// set mouseIsclicked to false
 		mouseIsClicked = false;
+		// make it visible
 		renderer.enabled = true;
+		// change the position from the screen's mouse position to the game screen's mouse position
 		transform.position = Camera.mainCamera.ScreenToWorldPoint(mousePos);
+		// create a particle explosion at the clicked spot
 		Instantiate (particleSystem, Camera.mainCamera.ScreenToWorldPoint(mousePos), transform.rotation);
 	}
 }
--- Assets/player_shank.cs
+++ Assets/player_shank.cs
@@ -1,21 +1,35 @@
 using UnityEngine;
 using System.Collections;
+// ABL38 - contrary to its name, the player_shank code is the main code used for every type of prisoner.
+// the type of prisoner is deemed by the public string character, where you either input shank, rock, or big to specify what type of prisoner it is
 
+// whenever you see other member's code, it is not my code. Most of the commented code found in here is my code.
 public class player_shank : MonoBehaviour {
+
+	// specifies what type of character it is
 	public string character;
+	// these booleans specify if these are being controlled or not
 	public static bool shank = false;
 	public static bool rock = false;
 	public static bool big = false;
+	// this specifies if the GUI is being selected, so the prisoner's wont walk towards the GUI when a button is clicked
 	public static bool gitSelected = false;
+	// contains the position of where the player has kicked
 	private Vector3 clickedTarget;
+	// if the character is walking or not
 	public bool walking;
+	// if this character is being controlled right now or not
 	public static bool controlled = false;
+	// these boolean values prevent any audio files from overlapping each other for each character
 	private bool allowedToPlayAudio;
 	private bool allowedToPlayWalkAudio = true;
+	// specifies the speed of the character
 	public float speed = 4;
+	// these contain an array of sounds in which a random sound is chosen each time to be palyed
 	public AudioClip[] radioGo = new AudioClip[9];
 	public AudioClip[] footsteps = new AudioClip[6];
-	public string player = "a";
+	
+	// other member's code
 	private bool keyIsDown = false;
 	public static int activeGUI = 0; //Used to prevent GUI selection automatically moving the player, Should only be set to 2 max.
 	public int HealthInitial = 100;
@@ -25,10 +39,12 @@ public class player_shank : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
+		// ABL38
+		// initialize field values
 		allowedToPlayAudio = true;
 		clickedTarget = transform.position;
     	walking = false;
-		
+		// Other member's code
 		//Initializes Health Counter
 		Health = HealthInitial;
 		if(player=="a"){
@@ -46,7 +62,8 @@ public class player_shank : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(activeGUI == 0){//Prevents unintended movement when using the GUI
+		// other group member's code
+		if(activeGUI == 0){ //Prevents unintended movement when using the GUI
 			SendMessage ("ResetPlayer");
 			SendMessage ("GetPlayerControl");
 			if(Input.GetKeyDown (player)){
@@ -55,20 +72,27 @@ public class player_shank : MonoBehaviour {
 			if(Input.GetKeyUp (player)){
 				keyIsDown = false;
 			}
+			// ABL38 - this is the start of my code
 			if ((Input.GetMouseButtonUp(0)) && ((controlled) || (keyIsDown)) && (!gitSelected)) {
+				// on the mouse button up, if the GUI button is clicked or its respective key is pressed, and the mouse is currently not on the GUI,
+				// then these actions are performed
 				mousePointer.mouseIsClicked = true;
+				// set the mousePointer's mouseIsClicked field to true
+				// if a radio sound is allowed to play, then call the function PlayRadioSound()
 				if(allowedToPlayAudio){
 					SendMessage ("PlayRadioSound");
 				}
-					// set the clickedTarget's position to that of the mouse
-					clickedTarget.x = Input.mousePosition.x;
-					clickedTarget.y = Input.mousePosition.y;
-					clickedTarget.z = transform.position.z - Camera.mainCamera.transform.position.z;
-					// walking is now true 
-					walking = true;
+				// set the clickedTarget's position to that of the mouse
+				clickedTarget.x = Input.mousePosition.x;
+				clickedTarget.y = Input.mousePosition.y;
+				clickedTarget.z = transform.position.z - Camera.mainCamera.transform.position.z;
+				// walking is now true 
+				walking = true;
 			}
 			if(walking){
+				// if the character is walking, the white circle indicator of where the mouse has been clicked has its visibility set to true
 				mousePointer.disappear = false;
+				// as the character is moving, there are no restraints for the rigidbody
 				rigidbody.constraints = RigidbodyConstraints.None;
 				// checks if the clickedTarget is far enough
 				// prevents the character from inefficiently moving a little bit
@@ -93,21 +117,31 @@ public class player_shank : MonoBehaviour {
 			activeGUI -= 1;
 		}		
 	}
-	
+	// ABL38
 	void OnCollisionEnter(Collision other){
-			mousePointer.disappear = true;
-			walking = false;
-			rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+		// when any character collides with anything, if the "mousepointer," the white circle indicating the position you clicked
+		// where you want the character to go to will disappear
+		mousePointer.disappear = true;
+		// stop the character from walking
+		walking = false;
+		// as you hit something, freeze the rigidbody to prevent it from moving
+		rigidbody.constraints = RigidbodyConstraints.FreezeAll;
 	}
-	
+	// ABL38
 	IEnumerator PlayRadioSound(){
+		// the radio sound is about to be played, so this is set to false 
 		allowedToPlayAudio = false;
+		// wait for 0.2f seconds so the click sound will not interfere with the radio sound
 		yield return new WaitForSeconds (0.2f);
+		// play a random radio sound from the array of sounds
 		audio.clip = radioGo[Random.Range(0, 9)];
 		audio.PlayOneShot(audio.clip);
+		// wait for how long the audio clip's length then set the allowedToPlayAudio to true to prevent
+		// multiple radio sounds from being played from the same character
 		yield return new WaitForSeconds (audio.clip.length);
 		allowedToPlayAudio = true;
 	}
+	// ABL38
 	IEnumerator PlayWalkSound(){
 		// the footstep sound is being played, so this is set to false
 		// prevents multiple footsteps from being played from the same person
@@ -116,7 +150,7 @@ public class player_shank : MonoBehaviour {
 		audio.PlayOneShot(footsteps[Random.Range(0, 6)]);
 		// the amount of seconds between each footstep sound
 		float footstepDelay = 0.35f;
-		// based on the character type, the footstep delay can be faster or slower
+		// based on the character type, the footstep sound delay can be faster or slower
 		if(character.Equals ("shank")){
 			footstepDelay = 0.35f;
 		}
@@ -131,7 +165,9 @@ public class player_shank : MonoBehaviour {
 		// then set this to true, so another footstep sound can be played in necessary
 		allowedToPlayWalkAudio = true;
 	}
-	
+	// ABL38
+	// When the character button on the GUI is clicked, this checks which type of player this code is assigned to
+	// and sets if that character is true or not based on the conditions
 	void GetPlayerControl(){
 		if((character.Equals ("shank")) && (shank)){
 			controlled = true;
@@ -152,12 +188,12 @@ public class player_shank : MonoBehaviour {
 			controlled = false;
 		}
 	}
-	
+	// Other group member's code
 	void ResetPlayer(){
 		transform.position = new Vector3(transform.position.x, transform.position.y, 0);
 		transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
 	}
-	
+	// Other group member's code
 	void Injured(string x){
 		//Damage
 		if(x == ("Guard")){
@@ -170,7 +206,7 @@ public class player_shank : MonoBehaviour {
 		}
 	}
 	
-	
+	// Other group member's code
 	void DeadPlayer(){
 		transform.position=new Vector3(-92.69604f, -32.47769f, 0);
 		Health= HealthInitial/2;
